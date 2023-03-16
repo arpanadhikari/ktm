cf43d0773db42f2ddb2aab7806b0babf264aec96
Arpan Adhikari <arpan@thephotostudio.com.au>
2023-03-17 00:55:15 +1100

    Added test case for WatchEvents

--- cmd/podwatch_test.go
+++ cmd/podwatch_test.go
@@ -9,6 +9,7 @@ podwatch_test.go is a test file for podwatch.
 package cmd
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -70,3 +71,56 @@ func TestPodWatch(t *testing.T) {
 }
 
 // TODO: Add test for podwatch function.
+func TestWatchEvents(t *testing.T) {
+	db, _ := OpenPodHistoryDB()
+	clientset := fake.NewSimpleClientset()
+	stop := make(chan struct{})
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- watchEvents(clientset, db, stop)
+	}()
+
+	// wait for informers to start
+	time.Sleep(1 * time.Second)
+
+	// create a new pod
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod-testwatchevents",
+			Namespace: "default",
+		},
+	}
+
+	// create a new node
+	node := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-node-testwatchevents",
+		},
+	}
+	_, err := clientset.CoreV1().Pods(pod.Namespace).Create(context.Background(), pod, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	_, err = clientset.CoreV1().Nodes().Create(context.Background(), node, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	// wait for the event to be processed
+	time.Sleep(1 * time.Second)
+
+	// check if the pod was added to the database
+	podHistory, err := db.GetPodHistory(pod.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, pod.Name, podHistory.Pod.Name)
+
+	// check if the node was added to the database
+	nodeHistory, err := db.GetNodeHistory(node.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, node.Name, nodeHistory.Node.Name)
+
+	// stop the watcher
+	close(stop)
+
+	// wait for the watcher to exit
+	err = <-errCh
+	assert.NoError(t, err)
+}
