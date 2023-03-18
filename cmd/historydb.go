4acec8420ae982e4478da2210ecab05f68d4c85f
Arpan Adhikari <arpan@thephotostudio.com.au>
2023-03-18 23:25:17 +1100

    more scenarios

--- cmd/historydb.go
+++ cmd/historydb.go
@@ -6,6 +6,8 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
 
@@ -18,9 +20,23 @@ type PodHistoryDB struct {
 }
 
 // OpenPodHistoryDB opens the podhistorydb database.
-func OpenPodHistoryDB(inMemory ...*bool) (*PodHistoryDB, error) {
+func OpenPodHistoryDB() (*PodHistoryDB, error) {
 	var db *bolt.DB
 	db, err := bolt.Open("ktm_podhistorydb.db", 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PodHistoryDB{
+		db: db,
+	}, nil
+
+}
+
+// OpenPodHistoryDBWithFile opens the podhistorydb database with a given file.
+func OpenPodHistoryDBWithFile(filename string) (*PodHistoryDB, error) {
+	var db *bolt.DB
+	db, err := bolt.Open(filename, 0600, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -68,38 +84,95 @@ func (phdb *PodHistoryDB) AddPodHistory(ph PodHistory) error {
 		if err != nil {
 			return err
 		}
-		// searialize the pod history
+		// Serialize the pod history
 		serialized, _ := json.Marshal(ph)
-		// use podname+event timestamp as the key, use creation timestamp if event timestamp is not available
-		if ph.Event.CreationTimestamp.IsZero() {
-			return b.Put([]byte(ph.Pod.ObjectMeta.Name+ph.Pod.ObjectMeta.CreationTimestamp.String()), serialized)
+		// Use podname#event timestamp as the key, use creation timestamp if event timestamp is not available
+		var timestamp string
+		if ph.Event.FirstTimestamp.IsZero() {
+			timestamp = ph.Pod.ObjectMeta.CreationTimestamp.Format(time.RFC3339Nano)
+		} else {
+			timestamp = ph.Event.FirstTimestamp.Format(time.RFC3339Nano)
 		}
-		return b.Put([]byte(ph.Pod.ObjectMeta.Name+ph.Event.CreationTimestamp.String()), serialized)
+		key := ph.Pod.ObjectMeta.Name + "#" + timestamp
+		// print the key
+		fmt.Printf("adding key=%s\n", key)
+		return b.Put([]byte(key), serialized)
+	})
+}
+
+func (phdb *PodHistoryDB) GetPodsRelativeTime(relativeTime string) ([]PodHistory, error) {
+	var ph []PodHistory
+
+	// Parse the relative time string into a duration
+	duration, err := time.ParseDuration(relativeTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// Calculate the earliest timestamp
+	earliestTime := time.Now().Add(-duration)
+
+	err = phdb.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("pods"))
+		if b == nil {
+			return nil
+		}
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// Find the separator position in the key
+			sepPos := bytes.IndexByte(k, '#')
+			if sepPos == -1 {
+				continue
+			}
+
+			// Extract the timestamp from the key
+			timestampBytes := k[sepPos+1:]
+
+			// Parse the timestamp string into a time.Time value
+			timestampStr := string(timestampBytes)
+			timestamp, err := time.Parse(time.RFC3339, timestampStr)
+			if err != nil {
+				return err
+			}
+
+			// Check if the timestamp of the event is after earliestTime
+			if timestamp.After(earliestTime) {
+				var p PodHistory
+				json.Unmarshal(v, &p)
+				ph = append(ph, p)
+			}
+		}
+		return nil
 	})
 
+	return ph, err
 }
 
 // GetPodHistory returns the pod history for a given pod.
 func (phdb *PodHistoryDB) GetPodHistory(podName string) ([]PodHistory, error) {
 	var ph []PodHistory
 
-	// retrieve all the pod history that starts with the pod name
+	// Retrieve all the pod history that starts with the pod name
 	err := phdb.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("pods"))
 		if b == nil {
 			return nil
 		}
 		c := b.Cursor()
-		for k, v := c.Seek([]byte(podName)); k != nil && bytes.HasPrefix(k, []byte(podName)); k, v = c.Next() {
+
+		prefix := []byte(podName + "#")
+		for k, v := c.Seek(prefix); k != nil && bytes.HasPrefix(k, prefix); k, v = c.Next() {
 			var p PodHistory
 			json.Unmarshal(v, &p)
 			ph = append(ph, p)
+			// print the key and value
+			fmt.Printf("key=%s, value=%s\n", k, v)
 		}
 		return nil
 	})
 
 	return ph, err
-
 }
 
 // AddNodeHistory add a nodes history to the database.
@@ -109,25 +182,37 @@ func (phdb *PodHistoryDB) AddNodeHistory(nh NodeHistory) error {
 		if err != nil {
 			return err
 		}
-		serialized, err := json.Marshal(nh)
-		if err != nil {
-			return err
+		// serialize the node history
+		serialized, _ := json.Marshal(nh)
+		// use nodename#event timestamp as the key, use creation timestamp if event timestamp is not available
+		var timestamp string
+		if nh.Event.FirstTimestamp.IsZero() {
+			timestamp = nh.Node.ObjectMeta.CreationTimestamp.Format(time.RFC3339Nano)
+		} else {
+			timestamp = nh.Event.FirstTimestamp.Format(time.RFC3339Nano)
 		}
-		return b.Put([]byte(nh.Node.ObjectMeta.Name), serialized)
+		key := nh.Node.ObjectMeta.Name + "#" + timestamp
+		return b.Put([]byte(key), serialized)
 	})
 }
 
 // GetNodeHistory returns the node history for a given node.
-func (phdb *PodHistoryDB) GetNodeHistory(nodeName string) (NodeHistory, error) {
-	var nh NodeHistory
+func (phdb *PodHistoryDB) GetNodeHistory(nodeName string) ([]NodeHistory, error) {
+	// retrieve all the node history that starts with the node name
+	var nh []NodeHistory
 	err := phdb.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("nodes"))
 		if b == nil {
 			return nil
 		}
-		// desearlize the node history
-		serialized := b.Get([]byte(nodeName))
-		json.Unmarshal(serialized, &nh)
+		c := b.Cursor()
+
+		prefix := []byte(nodeName)
+		for k, v := c.Seek(prefix); k != nil && bytes.HasPrefix(k, prefix); k, v = c.Next() {
+			var n NodeHistory
+			json.Unmarshal(v, &n)
+			nh = append(nh, n)
+		}
 		return nil
 	})
 	return nh, err
--- cmd/historydb_test.go
+++ cmd/historydb_test.go
@@ -9,6 +9,7 @@ podhistorydb_test.go is a test file for podhistorydb.
 package cmd
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -18,39 +19,81 @@ import (
 )
 
 // TestGetPods tests the getPods function
-func TestGetPods(t *testing.T) {
+func TestGetPodHistory(t *testing.T) {
 
-	podName := "test-pod-testgetpods"
-	podNamespace := "test-namespace"
+	withTestPodHistoryDB(t, func(phdb *PodHistoryDB, t *testing.T) {
 
-	db, err := OpenPodHistoryDB()
+		podName := "test-pod"
 
-	if err != nil {
-		t.Errorf("failed to open database: %v", err)
-	}
+		testData := []PodHistory{
+			// add pods only with name and creationtimestamp
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: podName, Namespace: "namespace1", CreationTimestamp: metav1.Time{Time: time.Now()}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: podName, Namespace: "namespace2", CreationTimestamp: metav1.Time{Time: time.Now()}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: podName, Namespace: "namespace3", CreationTimestamp: metav1.Time{Time: time.Now()}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: podName, Namespace: "namespace4", CreationTimestamp: metav1.Time{Time: time.Now()}}}},
+		}
 
-	// add podhistory to db
-	err = db.AddPodHistory(PodHistory{
-		Pod: v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:              podName,
-				Namespace:         podNamespace,
-				CreationTimestamp: metav1.Time{Time: time.Now()},
-			},
-		},
+		// Store the test data in the database
+		for _, ph := range testData {
+			err := phdb.AddPodHistory(ph)
+			assert.NoError(t, err)
+		}
+
+		// Get the pod history
+		podHistory, err := phdb.GetPodHistory(podName)
+		assert.NoError(t, err)
+
+		// assert number of pods
+		assert.Equal(t, len(testData), len(podHistory))
+
+		// Check if the pod history is correct
+		for _, ph := range podHistory {
+			fmt.Printf("Pod: %s, %s, %s\n", ph.Pod.ObjectMeta.Name, ph.Pod.ObjectMeta.Namespace, ph.Pod.ObjectMeta.CreationTimestamp.Format(time.RFC3339Nano))
+			assert.Equal(t, podName, ph.Pod.Name)
+		}
+
+		//close db
+		phdb.Close()
 	})
-	if err != nil {
-		t.Errorf("failed to add pod to database: %v", err)
-	}
 
-	podHistory, err := db.GetPodHistory(podName)
-	if err != nil {
-		t.Errorf("failed to get pod from database: %v", err)
-	}
+}
 
-	assert.Equal(t, podHistory.Pod.ObjectMeta.Name, podName, "pod name is not the same as the one we created")
+func TestGetPodsRelativeTime(t *testing.T) {
 
-	//close db
-	db.Close()
+	withTestPodHistoryDB(t, func(phdb *PodHistoryDB, t *testing.T) {
+		defer phdb.Close()
 
+		// Create sample PodHistory data
+		now := time.Now()
+		testData := []PodHistory{
+			// add pods only with name and creationtimestamp
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod1", CreationTimestamp: metav1.Time{Time: now.Add(-8 * time.Hour)}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod2", CreationTimestamp: metav1.Time{Time: now.Add(-6 * time.Hour)}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod3", CreationTimestamp: metav1.Time{Time: now.Add(-2 * time.Hour)}}}},
+			{Pod: v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod4", CreationTimestamp: metav1.Time{Time: now.Add(-1 * time.Hour)}}}},
+		}
+
+		// Store the test data in the database
+		for _, ph := range testData {
+			err := phdb.AddPodHistory(ph)
+			assert.NoError(t, err)
+		}
+
+		// Test GetPods function with different relative times
+		testCases := []struct {
+			relativeTime  string
+			expectedCount int
+		}{
+			{"9h", 4},
+			{"7h", 3},
+			{"3h20m", 2},
+			{"50m", 0},
+		}
+
+		for _, tc := range testCases {
+			pods, err := phdb.GetPodsRelativeTime(tc.relativeTime)
+			assert.NoError(t, err)
+			assert.Len(t, pods, tc.expectedCount, "Unexpected number of pods for relative time %s", tc.relativeTime)
+		}
+	})
 }
